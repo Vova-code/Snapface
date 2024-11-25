29af260e08f2ebfea61cb029ed1618a411414a39
Vova-code <vdoche@live.fr>
2024-11-25 11:02:17 +0100

    Chapter 1 : Observable operators

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {HeaderComponent} from './header/header.component';
 import {RouterOutlet} from '@angular/router';
 import {interval, Observable} from 'rxjs';
+import {map, filter, tap} from 'rxjs/operators';
 import {AsyncPipe} from '@angular/common';
 
 @Component({
@@ -17,9 +18,20 @@ import {AsyncPipe} from '@angular/common';
 })
 export class AppComponent implements OnInit {
 
-  interval$!: Observable<number>;
+  interval$!: Observable<string>;
 
   ngOnInit() {
-    this.interval$ = interval(1000);
+    this.interval$ = interval(1000).pipe(
+      filter(value => value % 3 === 0),
+      map(value => value % 2 === 0 ?
+        `Je suis ${value} et je suis Pair` :
+        `Je suis ${value} et je suis Impair`
+      ),
+      tap(value => this.logger(value))
+    );
+  }
+
+  logger(text: string): void {
+    console.log(`Log ${text}`)
   }
 }
