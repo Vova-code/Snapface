e4ed3c602e19b1e4ca9e9b736359106c7d4cacfb
Vova-code <vdoche@live.fr>
2024-11-25 10:49:36 +0100

    Chapter 1 : Observable introduction

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,25 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {HeaderComponent} from './header/header.component';
 import {RouterOutlet} from '@angular/router';
+import {interval, Observable} from 'rxjs';
+import {AsyncPipe} from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     HeaderComponent,
-    RouterOutlet
+    RouterOutlet,
+    AsyncPipe
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+
+  interval$!: Observable<number>;
+
+  ngOnInit() {
+    this.interval$ = interval(1000);
+  }
+}
