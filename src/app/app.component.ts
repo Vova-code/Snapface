db5c15cab1612ffced593952874f8414f17187e3
Vova-code <vdoche@live.fr>
2024-11-25 12:05:11 +0100

    Chapter 1 : High level operators

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {HeaderComponent} from './header/header.component';
 import {RouterOutlet} from '@angular/router';
-import {interval, Observable} from 'rxjs';
-import {map, filter, tap} from 'rxjs/operators';
+import {concatMap, delay, exhaustMap, interval, mergeMap, of, switchMap, take} from 'rxjs';
+import {map, tap} from 'rxjs/operators';
 import {AsyncPipe} from '@angular/common';
 
 @Component({
@@ -18,20 +18,31 @@ import {AsyncPipe} from '@angular/common';
 })
 export class AppComponent implements OnInit {
 
-  interval$!: Observable<string>;
+  redTrainsCalled = 0;
+  yellowTrainsCalled = 0;
 
   ngOnInit() {
-    this.interval$ = interval(1000).pipe(
-      filter(value => value % 3 === 0),
-      map(value => value % 2 === 0 ?
-        `Je suis ${value} et je suis Pair` :
-        `Je suis ${value} et je suis Impair`
-      ),
-      tap(value => this.logger(value))
-    );
+    interval(500).pipe(
+      take(10),
+      map(value => value % 2 === 0 ? 'rouge' : 'jaune'),
+      tap(color => console.log(`La lumière s'allume en %c${color}`, `color: ${this.translateColor(color)}`)),
+      switchMap(color => this.getTrainObservable$(color)),
+      tap(train => console.log(`Train %c${train.color} ${train.trainIndex} arrivé !`, `font-weight: bold; color: ${this.translateColor(train.color)}`)),
+    ).subscribe();
   }
 
-  logger(text: string): void {
-    console.log(`Log ${text}`)
+  translateColor(color: 'rouge' | 'jaune'): string {
+    return color === 'rouge' ? 'red' : 'yellow';
+  }
+
+  private getTrainObservable$(color: 'rouge' | 'jaune') {
+    const isRedTrain = color === 'rouge';
+    isRedTrain ? this.redTrainsCalled++ : this.yellowTrainsCalled++;
+    const trainIndex = isRedTrain ? this.redTrainsCalled : this.yellowTrainsCalled;
+    console.log(`Train %c${color} ${trainIndex} appelé !`, `font-decoration: underline; color: ${this.translateColor(color)}`);
+
+    return of({color, trainIndex}).pipe(
+        delay(isRedTrain ? 5000 : 6000),
+    );
   }
 }
