8f853c47f8196ab1ad8c0be40c043ff9ce6b3afc
Vova-code <vdoche@live.fr>
2024-12-05 11:53:36 +0100

    Chapter 3 : Secure requests with interceptors

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,15 +1,17 @@
-import {ApplicationConfig, importProvidersFrom, LOCALE_ID, provideZoneChangeDetection} from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {ApplicationConfig, LOCALE_ID, provideZoneChangeDetection} from '@angular/core';
+import {provideRouter} from '@angular/router';
 
-import { routes } from './app.routes';
+import {routes} from './app.routes';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {provideHttpClient, withInterceptorsFromDi} from '@angular/common/http';
+import {httpInterceptorProviders} from './interceptors';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideZoneChangeDetection({eventCoalescing: true}),
     provideHttpClient(withInterceptorsFromDi()),
     provideRouter(routes),
+    httpInterceptorProviders,
     {
       provide: LOCALE_ID,
       useValue: 'fr-FR',
